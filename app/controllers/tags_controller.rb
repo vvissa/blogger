df543332fee0358380ea028347baf0fe417a7a81
vvissa <dev.vvissa@gmail.com>
2017-02-23 04:32:38 +0000

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,17 @@
+class TagsController < ApplicationController
+    
+    def index
+        @tag = Tag.all
+    end 
+   
+    def show
+        @tag = Tag.find(params[:id])
+    end
+    
+    def destroy
+      @tag = Tag.find(params[:id])
+      @tag.destroy
+      flash.notice = "Tag'#{@tag.name}' Removed! Chale!!"
+      redirect_to articles_path
+    end
+end
